daaa23d50530925490716f380439f0ec2a969a06
shital bramhe <shitalbramhe@gmail.com>
2021-11-18 20:17:02 +0530

    [shital] ADD: contact created

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -8,6 +8,35 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            Contact newcontact = new Contact();
+            bool flag = true;
+
+            while (flag)
+            {
+                Console.WriteLine("Enter Number to Execute the Address book Program \n1. Create contacts \n2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the Contact Information first name, last names, address,city, state, zip, phone number and email id");
+                        newcontact.firstname = Console.ReadLine();
+                        newcontact.lastname = Console.ReadLine();
+                        newcontact.address = Console.ReadLine();
+                        newcontact.city = Console.ReadLine();
+                        newcontact.state = Console.ReadLine();
+                        newcontact.zip = Console.ReadLine();
+                        newcontact.phonenumber = Console.ReadLine();
+                        newcontact.emailid = Console.ReadLine();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+            }
         }
     }
 }
+ 
