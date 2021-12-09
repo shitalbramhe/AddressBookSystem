0c96895a251ad8ea7b5f2a23d7a29132d23938d7
shital bramhe <shitalbramhe@gmail.com>
2021-12-09 18:47:17 +0530

    [shital] ADD: view person by  city or state

--- AddressBookSystem/AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBookSystem/AddressBook.cs
@@ -11,6 +11,7 @@ namespace AddressBookSystem
         List<Contact> AddressList = new List<Contact>();
         Dictionary<string, List<Contact>> MultipleAddressbook = new Dictionary<string, List<Contact>>();
 
+
         public void AddContact(Contact newcontact)
         {
             AddressList.Add(newcontact);
@@ -146,7 +147,35 @@ namespace AddressBookSystem
 
                     foreach (Contact data in this.AddressList.FindAll(e => e.city == City))
                     {
-                        Console.WriteLine( data.firstname +" "+ data.lastname + " is from " + data.city);
+                        Console.WriteLine(data.firstname + " " + data.lastname + " is from " + data.city);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Enter State Name:");
+                    String State = Console.ReadLine();
+
+                    foreach (Contact data in this.AddressList.FindAll(e => e.state == State))
+                    {
+                        Console.WriteLine(data.firstname + " " + data.lastname + " is from " + data.state);
+                    }
+                    break;
+            }
+        }
+
+        public void view_person_city_state()
+        {
+            Console.WriteLine("Enter your Choice for Searching a Person in");
+            Console.WriteLine("\n1.City \n2.State");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter City Name:");
+                    String City = Console.ReadLine();
+
+                    foreach (Contact data in this.AddressList.FindAll(e => e.city == City))
+                    {
+                        Console.WriteLine(data.firstname + " " + data.lastname + " is from " + data.city);
                     }
                     break;
                 case 2:
@@ -163,9 +192,10 @@ namespace AddressBookSystem
 
 
         }
-
     }
+
 }
+
 
 
 
